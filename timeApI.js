a6a73d37c55e58052ff1477d7cf8e35e3537bb46
DanisonMabitsela <121928038+DanisonMabitsela@users.noreply.github.com>
2023-04-04 23:19:35 +0200

    Update timeApI.js

--- timeApI.js
+++ timeApI.js
@@ -1,15 +1,14 @@
-function updateTime() {
-  // Update the time on the webpage
-  let timeElement = document.getElementById("time");
-  //fetched time api
-  fetch("https://worldtimeapi.org/api/ip")
-    .then((response) => response.json())
-    .then((data) => {
-      let dateTimeData = data.datetime;
-      timeElement.innerHTML = dateTimeData;
-    })
-    .catch((error) => console.log(error));
+async function updateTime() {
+  try {
+    let timeElement = document.getElementById("time");
+    let response = await fetch("https://worldtimeapi.org/api/ip");
+    let data = await response.json();
+    let dateTimeData = data.datetime;
+    timeElement.innerHTML = dateTimeData;
+  } catch (error) {
+    console.log(error);
+  }
 }
-// Update the time every second
+
 setInterval(updateTime, 1000);
 updateTime();
